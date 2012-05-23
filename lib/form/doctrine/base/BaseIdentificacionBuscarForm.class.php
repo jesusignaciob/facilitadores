35daaca362141c9306fcd08341dbac38892f8940
jesusignaciob@gmail.com <jesusignaciob@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-23 22:36:31 +0000

    Agregado formulario de busqueda y todo lo relacionado, ajustes en tabla "identificacion" se cambio cedula de entero a string para manejar busquedas con like, agregado datos iniciales para estado, municipio y parroquia

---
+++ lib/form/doctrine/base/BaseIdentificacionBuscarForm.class.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Identificacion form base class.
+ *
+ * @method Identificacion getObject() Returns the current form's model object
+ *
+ * @package    facilitadores
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
+ */
+abstract class BaseIdentificacionBuscarForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'nombre'             => new sfWidgetFormInputText(),
+      'apellido'           => new sfWidgetFormInputText(),
+      'cedula_pasaporte'   => new sfWidgetFormInputText(),
+      'id_estado'          => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Estado'), 'add_empty' => true)),
+      'id_municipio'       => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Municipio'), 'add_empty' => true)),
+      'id_parroquia'       => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Parroquia'), 'add_empty' => true)),
+    ));
+
+
+    $this->widgetSchema->setNameFormat('identificacion[%s]');
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Identificacion';
+  }
+
+}
