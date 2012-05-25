3a908c485cd7f95fc65dd26fc3cc98576b3fcdce
jesusignaciob@gmail.com <jesusignaciob@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-25 01:24:51 +0000

    ajustes para corregir bugs

--- web/js/facilitadoresList.js
+++ web/js/facilitadoresList.js
@@ -1,22 +1,27 @@
-function cargarFacilitadores()
-{
-    var cedula = $('#identificacion_cedula_pasaporte').val();
-    var nombre = $('#identificacion_nombre').val();
-    var apellido = $('#identificacion_apellido').val();
-    var estado = $('#identificacion_id_estado option:selected').val();
-    if ( !(estado.lenght > 0) )
-        estado = 0;
-    var municipio = 0;
-    if (estado.lenght > 0)
-        municipio = $('#identificacion_id_municipio option:selected').val();
-    var parroquia = 0;
-    if (municipio.lenght > 0)
-        parroquia = $('#identificacion_id_parroquia option:selected').val();
+$(function() {
+  $('#identificacion_cedula_pasaporte').keyup(function(key) {
+    valNumero(this);
+  });
+});
 
-    $(function() {
-        $('#facilitadoresConsultados').load(
-          'cargarFacilitadores',
-          { 'cedula': cedula, 'nombre': nombre, 'apellido': apellido, 'estado': estado, 'municipio': municipio,  'parroquia': parroquia }
-        );
-    });
+function soloNumerico(variable) {
+  var numero = parseInt(variable);
+  if (isNaN(numero)) {
+    return "";
+  }
+  return numero;
+}
+
+function valNumero(control) {
+  control.value = soloNumerico(control.value);
+}
+
+function cargarFacilitadores(estado, municipio, parroquia) {
+  var cedula = $('#identificacion_cedula_pasaporte').val();
+  var nombre = $('#identificacion_nombre').val();
+  var apellido = $('#identificacion_apellido').val();
+
+  $(function() {
+    $('#facilitadoresConsultados').load('cargarFacilitadores',{ 'cedula': cedula, 'nombre': nombre, 'apellido': apellido, 'estado': estado, 'municipio': municipio,  'parroquia': parroquia });
+  });
 }
--- web/js/filtro_geo.js
+++ web/js/filtro_geo.js
@@ -1,21 +1,30 @@
+function estadoChange(idEstado) {
+  $(function() {
+    $('#td_municipio').load('cargarMunicipios', { query: idEstado }, function() {
+      $('#td_parroquia').html('');
+	    $('#identificacion_id_municipio').change(function(key) {
+        $('#municipio').attr('value', this.value);
+        var idMunicipio = (this.value=='' ? '*' : this.value);
+        municipioChange(idMunicipio);
+      });
+    });
+  });
+}
+
+function municipioChange(idMunicipio) {
+  $('#td_parroquia').load('cargarParroquias', { query: idMunicipio }, function() {
+    $('#identificacion_id_municipio').change(function(key) {
+      $('#parroquia').attr('value', this.value);
+    });
+  });
+}
+
 $(document).ready(function()
 {
   $('#identificacion_id_estado').change(function(key)
   {
-      var valor = (this.value=='' ? '*' : this.value)
-      $('#td_municipio').load(
-        'cargarMunicipios',
-        { query: valor}, function() {
-          $('#td_parroquia').html('');
-	  $('#identificacion_id_municipio').change(function(key)
-          {
-            var valor = (this.value=='' ? '*' : this.value)
-            $('#td_parroquia').load(
-              'cargarParroquias',
-              { query: valor}
-            );
-          });
-        }
-      );
+    $('#estado').attr('value', this.value);
+    var idEstado = (this.value=='' ? '*' : this.value)
+    estadoChange(idEstado);
   });
 });
