e7d4d59c99ddfb61288632002bd02ff1df874fa4
jesusignaciob@gmail.com <jesusignaciob@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-25 01:14:55 +0000

    ajustes para corregir bugs

--- trunk/apps/frontend/modules/gestion/templates/buscarSuccess.php
+++ trunk/apps/frontend/modules/gestion/templates/buscarSuccess.php
@@ -3,39 +3,39 @@
 <br>
 <br>
 <form action="<?php echo url_for('gestion/buscar'); ?>" method="post">
-<table style="border: 0">
-<tr>
-<td><?php echo $form['nombre']->renderLabel(); ?></td>
-<td><?php echo $form['nombre']->render(); ?></td>
-<td></td>
-</tr>
-<tr>
-  <td><?php echo $form['apellido']->renderLabel(); ?></td>
-  <td><?php echo $form['apellido']->render(); ?></td>
-  <td></td>
-</tr>
-<tr>
-  <td><?php echo $form['cedula_pasaporte']->renderLabel(); ?></td>
-  <td><?php echo $form['cedula_pasaporte']->render(); ?></td>
-  <td></td>
-</tr>
-<tr>
-  <td><?php echo $form['id_estado']->renderLabel(); ?></td>
-  <td><?php echo $form['id_estado']->render(); ?></td>
-  <td></td>
-</tr>
-<tr>
-  <td><?php echo $form['id_municipio']->renderLabel(); ?></td>
-  <td id="td_municipio"></td>
-  <td></td>
-</tr>
-<tr>
-  <td><?php echo $form['id_parroquia']->renderLabel(); ?></td>
-  <td id="td_parroquia"></td>
-  <td></td>
-</tr>
-</table>
-<input type="submit" onclick="cargarFacilitadores(); return false;" value="Consultar">
+  <table style="border: 0">
+    <tr>
+      <th style="text-align:right; height: 30px"><?php echo $form['nombre']->renderLabel(); ?></th>
+      <td><?php echo $form['nombre']->render(); ?></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th style="text-align:right; height: 30px"><?php echo $form['apellido']->renderLabel(); ?></th>
+      <td><?php echo $form['apellido']->render(); ?></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th style="text-align:right; height: 30px"><?php echo $form['cedula_pasaporte']->renderLabel(); ?></th>
+      <td><?php echo $form['cedula_pasaporte']->render(); ?></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th style="text-align:right; height: 30px"><?php echo $form['id_estado']->renderLabel(); ?></th>
+      <td><?php echo $form['id_estado']->render(); ?></td>
+      <td><input id="estado" value="" type="hidden"></td>
+    </tr>
+    <tr>
+      <th style="text-align:right; height: 30px"><?php echo $form['id_municipio']->renderLabel(); ?></th>
+      <td id="td_municipio"></td>
+      <td><input id="municipio" value="" type="hidden"></td>
+    </tr>
+    <tr>
+      <th style="text-align:right; height: 30px"><?php echo $form['id_parroquia']->renderLabel(); ?></th>
+      <td id="td_parroquia"></td>
+      <td><input id="parroquia" value="" type="hidden"></td>
+    </tr>
+  </table>
+<input type="submit" onclick="cargarFacilitadores($('#estado').val(), $('#municipio').val(), $('#parroquia').val()); return false;" value="Consultar">
 <?php echo $form->renderHiddenFields(); ?>
 </form>
 <br>
