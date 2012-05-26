9a27794dbf9584fd45bdb7f5a21ee575ea9bc502
jesusignaciob@gmail.com <jesusignaciob@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-26 16:30:09 +0000

    ajustes para permitir filtrar por areas de formacion y correccion de bugs

--- trunk/apps/frontend/modules/gestion/templates/_facilitadoresList.php
+++ trunk/apps/frontend/modules/gestion/templates/_facilitadoresList.php
@@ -5,33 +5,59 @@
   <input id="municipio" type="hidden" value="<?php echo $municipio; ?>" />
   <input id="parroquia" type="hidden" value="<?php echo $parroquia; ?>" />
   <input id="estatus" type="hidden" value="<?php echo $estatus; ?>" />
+  <input id="area" type="hidden" value="<?php echo $area; ?>" />
 
 <table border="1" style="border-collapse:collapse; width: 100%">
   <tr style="height: 30px">
-      <th>Acciones</th>
-      <th>Cédula</th>
-      <th>Nombres</th>
-      <th>Apellidos</th>
-      <th>Estado</th>
-      <th>Areas de Formacion</th>
+      <th rowspan="2">Acciones</th>
+      <th rowspan="2">Cédula</th>
+      <th rowspan="2">Nombres</th>
+      <th rowspan="2">Apellidos</th>
+      <th rowspan="2">Estado</th>
+      <th rowspan="1" colspan="2">Areas de Formacion</th>
+  </tr>
+  <tr>
+    <th>Nombre</th>
+    <th>Estatus</th>
   </tr>
   <?php foreach($facilitadores as $f): ?>
   <tr style="height: 30px">
       <td>
         [<a href="<?php echo url_for('gestion/detalle?id='.$f->getId()) ?>">Consultar<a>]
         [<a href="#">Modificar<a>]
-        [<a href="#" onclick="eliminarFacilitador($('#estado').val(), $('#municipio').val(), $('#parroquia').val(), $('#estatusAreaFormacion').val(), <?php echo $f->getId(); ?>); return false;">Eliminar<a>]
+        [<a href="#" onclick="eliminarFacilitador($('#estado').val(), $('#municipio').val(), $('#parroquia').val(), $('#estatus').val(), $('#area').val(), <?php echo $f->getId(); ?>); return false;">Eliminar<a>]
       </td>
       <td><?php echo $f->getCedulaPasaporte(); ?></td>
       <td><?php echo $f->getNombre(); ?></td>
       <td><?php echo $f->getApellido(); ?></td>
       <td><?php echo $f->getEstado(); ?></td>
-      <td>
-        <table border="0">
+      <td colspan="2">
+        <table border="0" style="width:100%">
           <?php foreach($f->getAreasFormacionFacilitador() as $aff): ?>
           <tr style="text-align:left; height: 15px">
-            <td><?php echo $aff->getAreasFormacion()->getNombreArea(); ?></td>
-            <td><?php echo $aff->getEstatus(); ?></td>
+            <td style="width:50%"><?php echo $aff->getAreasFormacion()->getNombreArea(); ?></td>
+            <td style="width:50%">
+              <?php
+                $x = $aff->getEstatus();
+                switch ($x)
+                {
+                  case 0:
+                    echo "En Formacion";
+                    break;
+                  case 1:
+                    echo "Formado";
+                    break;
+                  case 2:
+                    echo "Convocado";
+                    break;
+                  case 3:
+                    echo "Activo";
+                    break;
+                  default:
+                    echo "Inactivo";
+                }
+              ?>
+            </td>
           </tr>
           <?php endforeach; ?>
         </table>
--- trunk/apps/frontend/modules/gestion/templates/detalleSuccess.php
+++ trunk/apps/frontend/modules/gestion/templates/detalleSuccess.php
@@ -55,18 +55,45 @@
 <br>
 <table border="1" style="border-collapse:collapse; width: 100%">
   <tr>
-    <th>Areas de Formacion</th>
+    <th colspan="2">Areas de Formación</th>
+  </tr>
+  <tr>
+    <th>Nombre</th>
+    <th>Estatus</th>
   </tr>
   <?php foreach($areasFormPorFacilitador as $af): ?>
   <tr style="height: 30px">
     <td><?php echo $af->getAreasFormacion()->getNombreArea(); ?></td>
+    <td>
+      <?php
+        $x = $af->getEstatus();
+        switch ($x)
+        {
+          case 0:
+            echo "En Formación";
+            break;
+          case 1:
+            echo "Formado";
+            break;
+          case 2:
+            echo "Convocado";
+            break;
+          case 3:
+            echo "Activo";
+            break;
+          default:
+            echo "Inactivo";
+                }
+      ?>
+    </td>
   <tr>
   <?php endforeach; ?>
 </table>
-
+<br>
+<br>
 <table border="1" style="border-collapse:collapse; width: 100%">
   <tr>
-    <th>Telefonos</th>
+    <th>Teléfonos</th>
   </tr>
   <?php foreach($telefonos as $tels): ?>
   <tr style="height: 30px">
@@ -74,6 +101,8 @@
   <tr>
   <?php endforeach; ?>
 </table>
+<br>
+<br>
 <table border="1" style="border-collapse:collapse; width: 100%">
   <tr>
     <th>Correos</th>
@@ -83,4 +112,112 @@
     <td><?php echo $c->getCorreo(); ?></td>
   <tr>
   <?php endforeach; ?>
+</table>
+<br>
+<br>
+<table border="1" style="border-collapse:collapse; width: 100%">
+  <tr>
+    <th>Profesión</th>
+  </tr>
+  <?php foreach($facilitador->getProfesion() as $profesion): ?>
+  <tr style="height: 30px">
+    <td><?php echo $profesion->getNombreProfesion(); ?></td>
+  <tr>
+  <?php endforeach; ?>
+</table>
+<br>
+<br>
+<table border="1" style="border-collapse:collapse; width: 100%">
+  <tr>
+    <th>Ocupación</th>
+  </tr>
+  <?php foreach($facilitador->getOcupacion() as $ocupacion): ?>
+  <tr style="height: 30px">
+    <td><?php echo $ocupacion->getNombreOcupacion(); ?></td>
+  <tr>
+  <?php endforeach; ?>
+</table>
+<br>
+<br>
+<table border="1" style="border-collapse:collapse; width: 100%">
+  <tr>
+    <th>Nivel de Formación</th>
+  </tr>
+  <?php foreach($facilitador->getNivelFormacion() as $nivel): ?>
+  <tr style="height: 30px">
+    <td><?php echo $nivel->getEstudios()->getNombreEstudio(); ?></td>
+  <tr>
+  <?php endforeach; ?>
+</table>
+<br>
+<br>
+<table border="1" style="border-collapse:collapse; width: 100%">
+  <tr>
+    <th colspan="3">Disponibilidad de Traslado</th>
+  </tr>
+  <tr>
+    <th>Estado</th>
+    <th>Municipio</th>
+    <th>Requiere Traslado</th>
+  </tr>
+  <?php foreach($facilitador->getDisponibilidadTrasladoEstado() as $disponibilidad): ?>
+  <tr style="height: 30px">
+    <td><?php echo $disponibilidad->getEstado(); ?></td>
+    <td><?php echo $disponibilidad->getMunicipio(); ?></td>
+    <td>
+      <?php
+        $x = $disponibilidad->getRequiereTraslado();
+        switch ($x)
+        {
+          case 0:
+            echo "No";
+            break;
+          case 1:
+            echo "Si";
+            break;
+                }
+      ?>
+    </td>
+  <tr>
+  <?php endforeach; ?>
+</table>
+<br>
+<br>
+<table border="1" style="border-collapse:collapse; width: 100%">
+  <tr>
+    <th colspan="3">Secciones</th>
+  </tr>
+  <tr>
+    <th>Area de Formación</th>
+    <th>Sección</th>
+    <th>Ente</th>
+  </tr>
+  <?php foreach($facilitador->getSecciones() as $seccion): ?>
+  <tr style="height: 30px">
+    <td><?php echo $seccion->getAreasFormacion()->getNombreArea(); ?></td>
+    <td><?php echo $seccion->getNombreSeccion(); ?></td>
+    <td><?php echo $seccion->getNombreEnte(); ?></td>
+  <tr>
+  <?php endforeach; ?>
+</table>
+<br>
+<br>
+<table border="1" style="border-collapse:collapse; width: 100%">
+  <tr>
+    <th colspan="4">Disponibilidad Diaria</th>
+  </tr>
+  <tr>
+  <?php foreach($facilitador->getDisponibilidadDias() as $dia): ?>
+    <th><?php echo $dia->getDia(); ?></th>
+  <?php endforeach; ?>
+  </tr>
+  <tr style="height: 30px">
+  <?php foreach($facilitador->getDisponibilidadDias() as $dia): ?>
+    <td>
+      <?php foreach($dia->getDisponibilidadTurnos() as $turno): ?>
+        <?php echo $turno->getTurno(); ?><br>
+      <?php endforeach; ?>
+    </td>
+  <?php endforeach; ?>
+  <tr>
 </table>
