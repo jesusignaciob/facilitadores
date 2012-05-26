22656317cbdbdab6a551001531a60de813024a40
andrescerrada@gmail.com <andrescerrada@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-26 19:34:51 +0000

    Se realizó la tabla para mostrar los niveles de formación

--- trunk/apps/frontend/modules/gestion/templates/insertar_nivel_formacion_facilitadorSuccess.php
+++ trunk/apps/frontend/modules/gestion/templates/insertar_nivel_formacion_facilitadorSuccess.php
@@ -3,7 +3,7 @@
 <button class="back-button" onclick="javascript:parent.location='<?php echo url_for('gestion/insertar_areas_formacion_facilitador?id='.$id); ?>'">Regresar</button>
 <br>
 <h4>
-Configuración de Nivel de Formación
+Configuración de Nivel de Formación Facilitador
 </h4>
 <br>
 <form action="<?php echo url_for('gestion/CreateNivelFormacionFacilitador?id='.$id); ?>" method="post">
@@ -30,13 +30,21 @@ echo $form['id_identificacion']->render(array('value'=>$id));
   <tr style="height: 30px">
 <?php if (isset($nivel_formacion_facilitador)){
 ?>
-<th>Nivel de Formación del Facilitador</th>
+<th colspan='2'>Lista Nivel de Formación</th>
   </tr>
-<?php foreach($nivel_formacion_facilitador as $nff): ?>
+<tr>
+<th>
+Nº
+</th>
+<th>
+Nivel de Formación
+</th>
+</tr>
+<?php $cont=1; foreach($nivel_formacion_facilitador as $nff): ?>
   <tr style="height: 30px">
-     
+      <td><?php echo $cont; ?></td>
       <td><?php echo $nff->getEstudios()->getnombre_estudio(); ?></td>
-      
+      <?php $cont +=1; ?>
   </tr>
   <?php endforeach; 
 } 
