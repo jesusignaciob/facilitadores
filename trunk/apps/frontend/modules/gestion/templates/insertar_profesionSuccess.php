3e50f2c8482726121e30d566e77ab831a62226a2
andrescerrada@gmail.com <andrescerrada@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-26 11:35:59 +0000

    Realización de las consultas instantaneas

--- trunk/apps/frontend/modules/gestion/templates/insertar_profesionSuccess.php
+++ trunk/apps/frontend/modules/gestion/templates/insertar_profesionSuccess.php
@@ -20,7 +20,29 @@ Configuración de Profesión
 echo $form['id_identificacion']->render(array('value'=>$id));
 ?>
 </form>
-<form action="<?php echo url_for('gestion/insertarcorreo?id='.$id); ?>" method="post">
+<form action="<?php echo url_for('gestion/insertar_ocupacion?id='.$id); ?>" method="post">
 <input type="submit" value="Siguiente">
 </form>
-
+<br>
+<table border="1" style="border-collapse:collapse; width: 100%">
+  <tr style="height: 30px">
+<?php if (isset($profesion_facilitador)){
+?>
+<th colspan='2'>Profesión del Facilitador</th>
+</tr>
+<tr><th>Nº</th>
+<th>Profesión</th></tr>
+  </tr>
+<?php $cont = 1; foreach($profesion_facilitador as $pf): ?>
+  <tr style="height: 30px">
+      <td><?php echo $cont ?></td>
+      <td><?php echo $pf->getnombre_profesion(); ?></td>
+      <?php $cont += 1; ?>
+  </tr>
+  <?php endforeach; 
+} 
+else 
+{ 
+echo "Aún no haz configurado Profesión del Facilitador"; 
+} ?>
+</table>
