a5cf87053f90e40e683beea0aa3950b39a36fe9c
andrescerrada@gmail.com <andrescerrada@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-26 19:44:53 +0000

    Se agrego el campo id_parroquia

--- lib/form/doctrine/base/BaseDisponibilidadTrasladoEstadoForm.class.php
+++ lib/form/doctrine/base/BaseDisponibilidadTrasladoEstadoForm.class.php
@@ -19,15 +19,17 @@ abstract class BaseDisponibilidadTrasladoEstadoForm extends BaseFormDoctrine
       'id_identificacion' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Identificacion'), 'add_empty' => true)),
       'id_estado'         => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Estado'), 'add_empty' => true)),
       'id_municipio'      => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Municipio'), 'add_empty' => true)),
+      'id_parroquia'      => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Parroquia'), 'add_empty' => true)), 
       'requiere_traslado' => new sfWidgetFormInputCheckbox(),
     ));
 
     $this->setValidators(array(
       'id'                => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
       'id_identificacion' => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Identificacion'), 'required' => false)),
-      'id_estado'         => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Estado'), 'required' => false)),
-      'id_municipio'      => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Municipio'), 'required' => false)),
-      'requiere_traslado' => new sfValidatorBoolean(array('required' => false)),
+      'id_estado'         => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Estado'), 'required' => true)),
+      'id_municipio'      => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Municipio'), 'required' => true)),
+      'id_parroquia'      => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Parroquia'), 'required' => true)),
+      'requiere_traslado' => new sfValidatorBoolean(array('required' => true)),
     ));
 
     $this->widgetSchema->setNameFormat('disponibilidad_traslado_estado[%s]');
