11d5897cf028d00e60cc90de9eea55584b18339b
andrescerrada@gmail.com <andrescerrada@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-26 19:33:57 +0000

    Se organizo la tabla para mostrar las áreas de formación

--- trunk/apps/frontend/modules/gestion/templates/insertar_areas_formacion_facilitadorSuccess.php
+++ trunk/apps/frontend/modules/gestion/templates/insertar_areas_formacion_facilitadorSuccess.php
@@ -3,7 +3,7 @@
 <button class="back-button" onclick="javascript:parent.location='<?php echo url_for('gestion/insertarcorreo?id='.$id); ?>'">Regresar</button>
 <br>
 <h4>
-Configuración de Áreas de Formación
+Configuración de Áreas de Formación Facilitador
 </h4>
 <br>
 <form action="<?php echo url_for('gestion/CreateAreasFormacionFacilitador?id='.$id); ?>" method="post">
@@ -31,13 +31,21 @@ echo $form['id_identificacion']->render(array('value'=>$id));
   <tr style="height: 30px">
 <?php if (isset($areas_formacion_facilitador)){
 ?>
-<th>Áreas de Formación del Facilitador</th>
+<th colspan='2'>Listas de Formación</th>
   </tr>
-<?php foreach($areas_formacion_facilitador as $aff): ?>
+<tr>
+<th>
+Nº
+</th>
+<th>
+Áreas de Formación
+</th>
+</tr>
+<?php $cont=1; foreach($areas_formacion_facilitador as $aff): ?>
   <tr style="height: 30px">
-     
+      <td><?php echo $cont; ?></td>
       <td><?php echo $aff->getAreasFormacion()->getnombre_area(); ?></td>
-      
+      <?php $cont +=1; ?>
   </tr>
   <?php endforeach; 
 } 
