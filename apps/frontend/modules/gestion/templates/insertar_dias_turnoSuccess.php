9a5c1c0d518c24454779de2fccf1ef7cfd9de707
andrescerrada@gmail.com <andrescerrada@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-26 19:34:17 +0000

    Organización del Formulario

--- apps/frontend/modules/gestion/templates/insertar_dias_turnoSuccess.php
+++ apps/frontend/modules/gestion/templates/insertar_dias_turnoSuccess.php
@@ -9,18 +9,223 @@ Configuración de Turnos Disponibles
 <form action="<?php echo url_for('gestion/CreateTurnoDisponible?id='.$id); ?>" method="post">
 
 <table border=1>
+  <?php $form['turno']->renderLabel(); ?>
+  <?php $form['turno']->render(); ?>
+  <?php $form['turno']->renderError(); ?>
 <tr>
-  <td><?php echo $form['turno']->renderLabel(); ?></td>
-  <td><?php echo $form['turno']->render(); ?></td>
-  <td><?php echo $form['turno']->renderError(); ?></td>
+<th>
+Turnos
+</th>
+<th colspan='6'>
+Días
+</th>
 </tr>
+<tr>
+<th>
+<input  type='checkbox'>7am-8am
+</th>
+<th>
+<input type='checkbox'>Lunes
+</th>
+<th>
+<input  type='checkbox'>Martes
+</th>
+<th>
+<input type='checkbox'>Miercoles
+</th>
+<th>
+<input type='checkbox'>Jueves
+</th>
+<th>
+<input type='checkbox'>Viernes
+</th><th>
+<input type='checkbox'>Sábado
+</th>
+</tr>
+<tr>
+<th>
+<input type='checkbox'>8am-9am
+</th>
+<th>
+<input type='checkbox'>Lunes
+</th>
+<th>
+<input type='checkbox'>Martes
+</th>
+<th>
+<input type='checkbox'>Miercoles
+</th>
+<th>
+<input type='checkbox'>Jueves
+</th>
+<th>
+<input type='checkbox'>Viernes
+</th><th>
+<input type='checkbox'>Sábado
+</th>
+</tr>
+<tr>
+<th>
+<input type='checkbox'>9am-10am
+</th>
+<th>
+<input type='checkbox'>Lunes
+</th>
+<th>
+<input type='checkbox'>Martes
+</th>
+<th>
+<input type='checkbox'>Miercoles
+</th>
+<th>
+<input type='checkbox'>Jueves
+</th>
+<th>
+<input type='checkbox'>Viernes
+</th><th>
+<input type='checkbox'>Sábado
+</th>
+</tr>
+<tr>
+<th>
+<input type='checkbox'>10am-11am
+</th>
+<th>
+<input type='checkbox'>Lunes
+</th>
+<th>
+<input type='checkbox'>Martes
+</th>
+<th>
+<input type='checkbox'>Miercoles
+</th>
+<th>
+<input type='checkbox'>Jueves
+</th>
+<th>
+<input type='checkbox'>Viernes
+</th><th>
+<input type='checkbox'>Sábado
+</th>
+</tr>
+<tr>
+<th>
+<input type='checkbox'>11am-12pm
+</th>
+<th>
+<input type='checkbox'>Lunes
+</th>
+<th>
+<input type='checkbox'>Martes
+</th>
+<th>
+<input type='checkbox'>Miercoles
+</th>
+<th>
+<input type='checkbox'>Jueves
+</th>
+<th>
+<input type='checkbox'>Viernes
+</th><th>
+<input type='checkbox'>Sábado
+</th>
+</tr>
+<tr>
+<th>
+<input type='checkbox'>2pm-3pm
+</th>
+<th>
+<input type='checkbox'>Lunes
+</th>
+<th>
+<input type='checkbox'>Martes
+</th>
+<th>
+<input type='checkbox'>Miercoles
+</th>
+<th>
+<input type='checkbox'>Jueves
+</th>
+<th>
+<input type='checkbox'>Viernes
+</th><th>
+<input type='checkbox'>Sábado
+</th>
+</tr>
+<tr>
+<th>
+<input type='checkbox'>3pm-4pm
+</th>
+<th>
+<input type='checkbox'>Lunes
+</th>
+<th>
+<input type='checkbox'>Martes
+</th>
+<th>
+<input type='checkbox'>Miercoles
+</th>
+<th>
+<input type='checkbox'>Jueves
+</th>
+<th>
+<input type='checkbox'>Viernes
+</th><th>
+<input type='checkbox'>Sábado
+</th>
+</tr>
+<tr>
+<th>
+<input type='checkbox'>4pm-5pm
+</th>
+<th>
+<input type='checkbox'>Lunes
+</th>
+<th>
+<input type='checkbox'>Martes
+</th>
+<th>
+<input type='checkbox'>Miercoles
+</th>
+<th>
+<input type='checkbox'>Jueves
+</th>
+<th>
+<input type='checkbox'>Viernes
+</th><th>
+<input type='checkbox'>Sábado
+</th>
+</tr>
+<tr>
+<th>
+<input type='checkbox'>5pm-6pm
+</th>
+<th>
+<input type='checkbox'>Lunes
+</th>
+<th>
+<input type='checkbox'>Martes
+</th>
+<th>
+<input type='checkbox'>Miercoles
+</th>
+<th>
+<input type='checkbox'>Jueves
+</th>
+<th>
+<input type='checkbox'>Viernes
+</th><th>
+<input type='checkbox'>Sábado
+</th>
+</tr>
+
 </table>
 <input type="submit">
 <?php echo $form->renderHiddenFields(); 
 echo $form['id_identificacion']->render(array('value'=>$id));
 ?>
 </form>
-<form action="<?php echo url_for('gestion/insertar_traslado?id='.$id); ?>" method="post">
+<form action="<?php echo url_for('gestion/insertar_traslados?id='.$id); ?>" method="post">
 <input type="submit" value="Siguiente">
 </form>
 <br>
