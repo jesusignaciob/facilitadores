6e78684c346e2c90a4f7ddcd0d6d32d71ee42fb3
andrescerrada@gmail.com <andrescerrada@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-26 11:41:37 +0000

    Se agrego consulta personalizada por id_identificacion

--- lib/model/doctrine/NivelFormacionTable.class.php
+++ lib/model/doctrine/NivelFormacionTable.class.php
@@ -16,4 +16,12 @@ class NivelFormacionTable extends Doctrine_Table
     {
         return Doctrine_Core::getTable('NivelFormacion');
     }
+    public static function obtenerNivelFormacionPorFacilitador($idFacilitador)
+    {
+        $q = Doctrine_Query::create()
+                ->from('NivelFormacion nff')
+                ->where('nff.id_identificacion = ?', $idFacilitador);
+                
+        return $q->execute();
+    }
 }
