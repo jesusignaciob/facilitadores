4ca3aeb889b6be59561b8cf9944df0c04cfa2183
joseph2283@gmail.com <joseph2283@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-27 03:01:46 +0000

    nuevas imagenes

--- trunk/web/js/filtro_geo.js
+++ trunk/web/js/filtro_geo.js
@@ -1,30 +1,21 @@
-function estadoChange(idEstado) {
-  $(function() {
-    $('#td_municipio').load('cargarMunicipios', { query: idEstado }, function() {
-      $('#td_parroquia').html('');
-	    $('#identificacion_id_municipio').change(function(key) {
-        $('#municipio').attr('value', this.value);
-        var idMunicipio = (this.value=='' ? '*' : this.value);
-        municipioChange(idMunicipio);
-      });
-    });
-  });
-}
-
-function municipioChange(idMunicipio) {
-  $('#td_parroquia').load('cargarParroquias', { query: idMunicipio }, function() {
-    $('#identificacion_id_municipio').change(function(key) {
-      $('#parroquia').attr('value', this.value);
-    });
-  });
-}
-
 $(document).ready(function()
 {
   $('#identificacion_id_estado').change(function(key)
   {
-    $('#estado').attr('value', this.value);
-    var idEstado = (this.value=='' ? '*' : this.value)
-    estadoChange(idEstado);
+      var valor = (this.value=='' ? '*' : this.value)
+      $('#td_municipio').load(
+        'cargarMunicipios',
+        { query: valor}, function() {
+          $('#td_parroquia').html('');
+	  $('#identificacion_id_municipio').change(function(key)
+          {
+            var valor = (this.value=='' ? '*' : this.value)
+            $('#td_parroquia').load(
+              'cargarParroquias',
+              { query: valor}
+            );
+          });
+        }
+      );
   });
 });
