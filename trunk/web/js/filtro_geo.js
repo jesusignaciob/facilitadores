836e2b4ba25d7d67e24d7e66465533f56639bf1d
jesusignaciob@gmail.com <jesusignaciob@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-30 03:40:55 +0000

    ajustes para solventar problema al pintar los controles de estado, municipio y parroquia al modificar e insertar

--- trunk/web/js/filtro_geo.js
+++ trunk/web/js/filtro_geo.js
@@ -1,18 +1,18 @@
-function estadoChange(idEstado) {
+function estadoChange(idEstado, path) {
   $(function() {
-    $('#td_municipio').load('cargarMunicipios', { query: idEstado }, function() {
+    $('#td_municipio').load(path + 'cargarMunicipios', { query: idEstado }, function() {
       $('#td_parroquia').html('');
 	    $('#identificacion_id_municipio').change(function(key) {
         $('#municipio').attr('value', this.value);
         var idMunicipio = (this.value=='' ? '*' : this.value);
-        municipioChange(idMunicipio);
+        municipioChange(idMunicipio, path);
       });
     });
   });
 }
 
-function municipioChange(idMunicipio) {
-  $('#td_parroquia').load('cargarParroquias', { query: idMunicipio }, function() {
+function municipioChange(idMunicipio, path) {
+  $('#td_parroquia').load(path + 'cargarParroquias', { query: idMunicipio }, function() {
     $('#identificacion_id_municipio').change(function(key) {
       $('#parroquia').attr('value', this.value);
     });
@@ -21,10 +21,22 @@ function municipioChange(idMunicipio) {
 
 $(document).ready(function()
 {
+  if ($('#identificacion_id').attr('value') == "")
+  {
+    $('#td_municipio').html('');
+    $('#td_parroquia').html('');
+  }
+
   $('#identificacion_id_estado').change(function(key)
   {
     $('#estado').attr('value', this.value);
     var idEstado = (this.value=='' ? '*' : this.value)
-    estadoChange(idEstado);
+    $('#td_parroquia').html('');
+    if ($('#identificacion_id').attr('value') != "")
+    {
+      estadoChange(idEstado, "../../");
+    }
+    else
+      estadoChange(idEstado, "");
   });
 });
