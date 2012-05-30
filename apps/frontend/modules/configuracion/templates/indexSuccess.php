d6a852801fe97393a56d8201a61daa21f42d42e8
joseph2283@gmail.com <joseph2283@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-30 15:37:15 +0000

    seguridad parcial II

--- apps/frontend/modules/configuracion/templates/indexSuccess.php
+++ apps/frontend/modules/configuracion/templates/indexSuccess.php
@@ -3,9 +3,6 @@
   <button class="cajaicono" onclick="javascript:parent.location='<?php echo url_for('users/index'); ?>'"><?php echo image_tag(''); ?><br/>
   Permisos
 </button>
-  <button class="cajaicono" onclick="javascript:parent.location='<?php echo url_for('geografia/index'); ?>'"><?php echo image_tag(''); ?><br/>
-  Datos Geográficos 
-</button>
   <button class="cajaicono" onclick="javascript:parent.location='<?php echo url_for('formacion/index')?>'"><?php echo image_tag('formacion.png'); ?><br/>
   Datos de formación 
 </button>
