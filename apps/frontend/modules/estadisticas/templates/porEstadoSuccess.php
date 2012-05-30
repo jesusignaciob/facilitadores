aae8af72ff7fbf5f5cbbc2d85d6e2ea96369ce62
jesusignaciob@gmail.com <jesusignaciob@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-05-30 18:31:29 +0000

    para manejar graficas por Estados

---
+++ apps/frontend/modules/estadisticas/templates/porEstadoSuccess.php
@@ -0,0 +1,39 @@
+<button class="back-button" onclick="javascript:parent.location='<?php echo url_for('principal/index'); ?>'">Ir al menú Principal</button>
+<button class="back-button" onclick="javascript:parent.location='<?php echo url_for('estadisticas/index'); ?>'">Regresar</button>
+
+<div id="dialog-confirm" title="Eliminar Facilitador" style="display: none; min-height: 100px;">
+	<p><span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 20px 0;"></span>¿Esta seguro de eliminar al Facilitador?</p>
+</div>
+
+<br>
+<br>
+
+<form action="<?php echo url_for('estadisticas/generarGrafico?estatusAreaFormacion=0'); ?>" method="post">
+  <table style="border: 0">
+    <tr>
+      <th style="text-align:right; height: 30px">Estatus</th>
+      <td>
+        <select id="estatus">
+          <option value=""></option>
+          <option value="0">En Formacion</option>
+          <option value="1">Formado</option>
+          <option value="2">Convocado</option>
+          <option value="3">Activo</option>
+          <option value="4">Inactivo</option>
+        </select>
+      </td>
+      <td><input id="estatusAreaFormacion" value="" type="hidden"></td>
+    </tr>
+  </table>
+  <input type="submit" value="Generar Grafico">
+</form>
+<br>
+<br>
+<P>
+  <?php
+  if (isset($estatusValor)) {
+    //echo '@grafico_por_estados?estatus=$estatusValor';
+    stOfc::createChart('90%', 450, '@grafico_por_estados?estatus=$estatusValor', false); 
+  }
+  ?>
+</P>
