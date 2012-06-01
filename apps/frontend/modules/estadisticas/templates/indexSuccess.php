a123fedd9de54ac26ef95c3d398d0a652afd70f1
jesusignaciob@gmail.com <jesusignaciob@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-06-01 16:25:19 +0000

    ajustes del look & feel

--- apps/frontend/modules/estadisticas/templates/indexSuccess.php
+++ apps/frontend/modules/estadisticas/templates/indexSuccess.php
@@ -1,4 +1,5 @@
 <button class="back-button" onclick="javascript:parent.location='<?php echo url_for('principal/index'); ?>'">Ir al menú Principal</button>
-<p><button class="" onclick="javascript:parent.location='<?php echo url_for('estadisticas/porEstado'); ?>'">Por Estado</button></p>
-<p><button class="" onclick="javascript:parent.location='<?php echo url_for('estadisticas/circular'); ?>'">Por Especialidad (Área de formación)</button></p>
-<p><button class="" onclick="javascript:parent.location='<?php echo url_for('estadisticas/barra'); ?>'">Por ente</button></p>
+<br>
+<p><button class="note" onclick="javascript:parent.location='<?php echo url_for('estadisticas/porEstado'); ?>'">Por Estado</button></p>
+<p><button class="note" onclick="javascript:parent.location='<?php echo url_for('estadisticas/circular'); ?>'">Por Especialidad (Área de formación)</button></p>
+<p><button class="note" onclick="javascript:parent.location='<?php echo url_for('estadisticas/barra'); ?>'">Por ente</button></p>
