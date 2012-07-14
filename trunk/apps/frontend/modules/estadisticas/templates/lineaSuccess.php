acb1add25389f2aa572452d1d80bd3234444184d
andrescerrada@gmail.com <andrescerrada@gmail.com@b504f894-e440-ae9f-2c89-881bbc70c37e>
2012-07-14 15:32:32 +0000

    Documentación del código fuente.

--- trunk/apps/frontend/modules/estadisticas/templates/lineaSuccess.php
+++ trunk/apps/frontend/modules/estadisticas/templates/lineaSuccess.php
@@ -1,1 +1,16 @@
+<!--
+Document / Documento: lineaSuccess del Módulo Estadística 
+
+Created on / Creado : 23/05/2012, 10:39:10 AM
+
+Author / Desarrolladores:
+1. Raúl Lobo 04267711578 andrescerrada@gmail.com
+2. José Ruiz 04265752819 jruiz@cenditel.gob.ve joseph2283@gmail.com
+3. Jesús Becerra 04263779960 jbecerra@cenditel.gob.ve jesusignaciob@gmail.com
+4. Rodolfo Sumoza 04166342086 rsumoza@cenditel.gob.ve rsumoza@gmail.com
+
+Description / Comentarios:
+Este archivo genera las siguientes funciones:
+1- Generar las estadísticas por Gráfico lineal.
+!-->
 <P><?php stOfc::createChart('90%', 450, '@grafico_linea', false); ?></P>
